76dad4f9d73ea214f10e6b8b70a7fc751ba6db30
henick408 <marcel.snopkowski@gmail.com>
2026-09-08 23:38:53 +0200

    Service provider and basic supabase auth methods

--- Program.cs
+++ Program.cs
@@ -1,8 +1,38 @@
-﻿namespace PasswordManager;
+﻿using System.Security.Cryptography;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PasswordManager.Dto;
+using PasswordManager.Service;
 
-static class Program
+namespace PasswordManager;
+
+public static class Program
 {
-    static void Main()
+    private static async Task<ServiceProvider> ConfigureSupabase()
     {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+        string url = configuration["Supabase:Url"]!;
+        string? publishableKey = configuration["Supabase:PublishableKey"];
+        Supabase.SupabaseOptions supabaseOptions = new()
+        {
+            AutoRefreshToken = true,
+            AutoConnectRealtime = true
+        };
+        Supabase.Client supabaseClient = new(url, publishableKey, supabaseOptions);
+
+        ServiceCollection services = new();
+        services.AddSingleton(supabaseClient);
+        services.AddSingleton<AuthService>();
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        await supabaseClient.InitializeAsync();
+        return serviceProvider;
+    }
+    public static async Task Main()
+    {
+
     }
 }
---
+++ Service/AuthService.cs
@@ -0,0 +1,31 @@
+using System.Buffers.Text;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using PasswordManager.Dto;
+using Supabase.Gotrue;
+
+namespace PasswordManager.Service;
+
+public class AuthService(Supabase.Client supabase)
+{
+    public async Task<Session?> SignUp(UserRequest request)
+    {
+        Session? session = await supabase.Auth.SignUp(request.Email, request.Password);
+        return session;
+    }
+
+    public async Task<Session?> SignIn(UserRequest request)
+    {
+        Session? session = await supabase.Auth.SignIn(request.Email, request.Password);
+        return session;
+    }
+
+    public User? GetCurrentUser()
+    {
+        return supabase.Auth.CurrentUser;
+    }
+
+}
