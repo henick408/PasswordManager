cf6e9e6a8f201e48f1a06d4ced6821fabdc42de7
henick408 <marcel.snopkowski@gmail.com>
2026-09-18 16:48:45 +0200

    MainViewModel Categories from viewmodel, passwords from viewmodel (mock data) Details panel shows details of the selected password

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using PasswordManager.Dto;
+
+namespace PasswordManager.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    private string selectedCategory;
+    private ObservableCollection<PasswordControlViewModel> passwords;
+    private PasswordControlViewModel selectedPassword;
+    
+    public IList<string> Categories { get; } = new List<string>
+    {
+        "Social", "Work", "Finance", "Shopping", "Entertainment", "Other"
+    };
+
+    public string SelectedCategory
+    {
+        get => selectedCategory;
+        set => SetField(ref selectedCategory, value);
+    }
+
+    public PasswordControlViewModel SelectedPassword
+    {
+        get => selectedPassword;
+        set => SetField(ref selectedPassword, value);
+    }
+
+    private IList<PasswordEntry> entries = new List<PasswordEntry>
+    {
+        new PasswordEntry
+        {
+            Url = "github.com",
+            Name = "Github",
+            Username = "github@mail.com",
+            Category = "Social",
+            Notes = "hujhujhuj"
+        },
+        new PasswordEntry
+        {
+            Url = "google.com",
+            Name = "Google",
+            Username = "google@mail.com"
+        },
+        new PasswordEntry
+        {
+            Url = "facebook.com",
+            Name = "Facebook",
+            Username = "facebook@mail.com"
+        },
+        new PasswordEntry
+        {
+            Url = "usos.com",
+            Name = "Usos",
+            Username = "usos@mail.com"
+        },
+    };
+
+    public ObservableCollection<PasswordControlViewModel> Passwords
+    {
+        get => passwords;
+        set => SetField(ref passwords, value);
+    } 
+
+    public MainViewModel()
+    {
+        Passwords = new ObservableCollection<PasswordControlViewModel>(entries.Select(pass => new PasswordControlViewModel(pass))
+            .ToList());
+    }
+}
--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -7,45 +7,11 @@ namespace PasswordManager;
 
 public partial class MainWindow : Window
 {
-    public ObservableCollection<PasswordEntry> MojePrzyciski { get; set; } = new ObservableCollection<PasswordEntry>
-    {
-        new PasswordEntry
-        {
-            Url = "github.com",
-            Name = "Github",
-            Username = "github@mail.com"
-        },
-        new PasswordEntry
-        {
-            Url = "google.com",
-            Name = "Google",
-            Username = "google@mail.com"
-        },
-        new PasswordEntry
-        {
-            Url = "facebook.com",
-            Name = "Facebook",
-            Username = "facebook@mail.com"
-        },
-        new PasswordEntry
-        {
-            Url = "usos.com",
-            Name = "Usos",
-            Username = "usos@mail.com"
-        },
-    };
-    
-    public ObservableCollection<PasswordControlViewModel> Passwords { get; set; }
+    private readonly MainViewModel viewModel = new MainViewModel();
 
     public MainWindow()
     {
         InitializeComponent();
-
-
-        Passwords  = new ObservableCollection<PasswordControlViewModel>(MojePrzyciski.Select(password => new PasswordControlViewModel(password)).ToList());
-
-        this.DataContext = this;
+        this.DataContext = viewModel;
     }
-    
-    
 }
