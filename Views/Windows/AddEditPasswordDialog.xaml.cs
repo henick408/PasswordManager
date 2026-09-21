c6f11211ad34ba46576c93c367d04cb6ad218157
henick408 <marcel.snopkowski@gmail.com>
2026-09-21 19:39:32 +0200

    Add/edit password window mock

---
+++ Views/Windows/AddEditPasswordDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using PasswordManager.Dto;
+using PasswordManager.ViewModels;
+
+namespace PasswordManager.Views.Windows;
+
+public partial class AddEditPasswordDialog : Window
+{
+
+    public AddEditPasswordDialog()
+    {
+        InitializeComponent();
+        
+    }
+    
+    private void GeneratorExpander_OnClick(object sender, RoutedEventArgs e)
+    {
+        GeneratorExpander.IsExpanded = !GeneratorExpander.IsExpanded;
+
+        GenerateButton.Content = !GeneratorExpander.IsExpanded ? "Generate" : "Hide";
+    }
+
+    private void SaveChangesButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void RevealPasswordButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        
+    }
+}
